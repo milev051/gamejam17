0d61bf1abe27712c10c437ec42120fc11395c9f8
Marko Milev <milev051@gmail.com>
2023-06-04 03:25:00 +0200

    Light

--- Scenes/LightEnemy.cs
+++ Scenes/LightEnemy.cs
@@ -3,66 +3,65 @@ using System;
 
 public class LightEnemy : KinematicBody
 {
-	// Declare member variables here. Examples:
-	// private int a = 2;
-	// private string b = "text";
-	Vector3 moveVector;
-	float speed;
-	float health;
-	bool hit;
-	PlayerDepth player;
-	Sprite3D sprite;
-	AnimationPlayer animPlayer;
-	Timer timer;
-	Area area;
-	AudioStreamPlayer3D audioPlayer;
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		player = GetParent().GetNode<PlayerDepth>("Player");
-		sprite = GetNode<Sprite3D>("Sprite3D");
-		animPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
-		timer = GetNode<Timer>("Timer");
-		area = GetNode<Area>("Area");
-		audioPlayer = GetNode<AudioStreamPlayer3D>("AudioStreamPlayer3D");
-		speed = 5.0f;
-		sprite.Visible = false;
-	}
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+    Vector3 moveVector;
+    float speed;
+    float health;
+    bool hit;
+    PlayerDepth player;
+    Sprite3D sprite;
+    CSGSphere sphere;
+    AnimationPlayer animPlayer;
+    Timer timer;
+    Area area;
+    AudioStreamPlayer3D audioPlayer;
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        player = GetParent().GetNode<PlayerDepth>("Player");
+        sprite = GetNode<Sprite3D>("Sprite3D");
+        sphere = GetNode<CSGSphere>("CSGSphere");
+        animPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+        timer = GetNode<Timer>("Timer");
+        area = GetNode<Area>("Area");
+        audioPlayer = GetNode<AudioStreamPlayer3D>("AudioStreamPlayer3D");
+        speed = 5.0f;
+        sprite.Visible = false;
+        sphere.Visible = true;
+    }
 
-	//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-	//  public override void _Process(float delta)
-	//  {
-	//      
-	//  }
-	// JEBITE STE SVI
-	public override void _PhysicsProcess(float delta)
-	{
-		LookAt(player.Translation, new Vector3(0.0f, 1.0f, 1.0f));
-		moveVector = (player.Translation - GlobalTransform.origin).Normalized();
-		float distance = moveVector.DistanceTo(player.Translation);
+    public override void _PhysicsProcess(float delta)
+    {
+        LookAt(player.Translation, new Vector3(0.0f, 1.0f, 1.0f));
+        moveVector = (player.Translation - GlobalTransform.origin).Normalized();
+        float distance = moveVector.DistanceTo(player.Translation);
 
-		MoveAndSlide(moveVector*speed);
-	}
+        MoveAndSlide(moveVector * speed);
+    }
 
-	public void _on_Area_body_entered(Node body)
-	{
-		if(body.GetType() == typeof(PlayerDepth))
-		{
-			PlayerDepth bodyPlayer = (PlayerDepth)body;
-			bodyPlayer.damage(10.0f);
-			sprite.Visible = true;
-			speed = 10.0f;
-			timer.Start();
-			audioPlayer.Play();
-			area.SetDeferred("monitoring", false);
-		}
-	}
+    public void _on_Area_body_entered(Node body)
+    {
+        if (body.GetType() == typeof(PlayerDepth))
+        {
+            PlayerDepth bodyPlayer = (PlayerDepth)body;
+            bodyPlayer.damage(10.0f);
+            sprite.Visible = true;
+            sphere.Visible = false;
+            speed = 10.0f;
+            timer.Start();
+            audioPlayer.Play();
+            area.SetDeferred("monitoring", false);
+        }
+    }
 
-	public void _on_Timer_timeout()
-	{
-		speed = 5.0f;
-		sprite.Visible = false;
-		audioPlayer.Stop();
-		area.SetDeferred("monitoring", true);
-	}
+    public void _on_Timer_timeout()
+    {
+        speed = 5.0f;
+        sprite.Visible = false;
+        sphere.Visible = true;
+        audioPlayer.Stop();
+        area.SetDeferred("monitoring", true);
+    }
 }
